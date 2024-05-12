ace3790b1954842950d92fbc3589a5a274280bb4
Ashakuur1 <engshakrayare114@gmail.com>
2024-05-12 17:30:51 +0300

    feat: Add user routes to server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user-Routes.js";
 const app = express();
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
+
+
+app.use("/users",userRoutes)
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT} `);
 });
